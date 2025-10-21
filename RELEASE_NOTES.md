8e0fe23fb0eac1f51a631d6b2527e02f0e89fbfd
Tim Forkmann <tim.forkmann@kraftwerk.io>
2025-10-21 07:41:56 -0400

    Bumping version to 0.0.9

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,5 @@
+#### 0.0.9 - 2025-10-21
+* Remove Fable.Browser.Svg dependency
 #### 0.0.8 - 2025-10-21
 * Migrate to Reactour
 #### 0.0.7 - 2025-09-18
--- src/Docs/Pages/ReactourView.fs
+++ src/Docs/Pages/ReactourView.fs
@@ -2,49 +2,76 @@ module Docs.Pages.ReactourView
 
 open Feliz
 open Feliz.Bulma
-open Feliz.QRCode
+open Feliz.Reactour
 open Docs.SharedView
 
 [<ReactComponent>]
-let QRCodeCanvas () =
-    QRCode.qrCodeCanvas [
-        qrCodeCanvas.value "https://www.google.com"
-        qrCodeCanvas.size 600
-        qrCodeCanvas.bgColor "#ffffff"
-        qrCodeCanvas.fgColor "#000000"
-        qrCodeCanvas.level "L"
-        qrCodeCanvas.includeMargin false
-        qrCodeCanvas.imageSettings [
-            imageSettingsCanvas.src "https://msuecar.azureedge.net/logos/favicon-32x32.png"
-            imageSettingsCanvas.height 24
-            imageSettingsCanvas.width 24
-            imageSettingsCanvas.excavate true
+let MyComponent () =
+    let tour = TourControls.useTour()
+    Html.div [
+        prop.children [
+            Html.section [
+                prop.className "section"
+                prop.children [
+                    Html.h1 [ prop.className "title"; prop.text "Feliz + Bulma + Reactour" ]
+                    Html.p [ prop.className "subtitle"; prop.text "A guided tour example using Reactour." ]
+
+                    Html.button [
+                        prop.className "button is-primary my-button"
+                        prop.text "Start Tour"
+                        prop.onClick (fun _ -> tour.setIsOpen true)
+                    ]
+
+                    Html.div [
+                        prop.className "box highlighted-section"
+                        prop.text "This is a highlighted Bulma box."
+                    ]
+                ]
         ]
     ]
+    ]
 
-let QRCode =
+[<ReactComponent>]
+let Reactour () =
+    TourProvider.tourProvider [
+        reactour.steps [
+            steps.step [
+                step.selector ".my-button"
+                step.content "Click this button to start your tour!"
+            ]
+            steps.step [
+                step.selector ".highlighted-section"
+                step.content "This is a highlighted section."
+            ]
+        ]
+        reactour.children [
+            MyComponent()
+        ]
+    ]
+let ReactourContainer =
     Html.div [
         prop.style [ style.height 800 ]
         prop.children [
-            QRCodeCanvas()
+            Reactour ()
         ]
     ]
 
 
 let code =
     """
-    QRCode.qrCodeCanvas [
-        qrCodeCanvas.value "https://www.google.com"
-        qrCodeCanvas.size 600
-        qrCodeCanvas.bgColor "#ffffff"
-        qrCodeCanvas.fgColor "#000000"
-        qrCodeCanvas.level "L"
-        qrCodeCanvas.includeMargin false
-        qrCodeCanvas.imageSettings [
-            imageSettingsCanvas.src "https://msuecar.azureedge.net/logos/favicon-32x32.png"
-            imageSettingsCanvas.height 24
-            imageSettingsCanvas.width 24
-            imageSettingsCanvas.excavate true
+    TourProvider.tourProvider [
+        reactour.steps [
+            steps.step [
+                step.selector ".my-button"
+                step.content "Click this button to start your tour!"
+            ]
+            steps.step [
+                step.selector ".highlighted-section"
+                step.content "This is a highlighted section."
+            ]
+        ]
+        reactour.children [
+            MyComponent()
         ]
     ]
     """
@@ -55,7 +82,7 @@ let title = Html.text "QR Code"
 let QRCodeView () =
     Html.div [
         Bulma.content [
-            codedView title code QRCode
+            codedView title code ReactourContainer
         ]
-        fixDocsView "QRCode" false
+        fixDocsView "Reactour" false
     ]
